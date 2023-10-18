edbf23a0333e829a261d63cc0df81ca881ecb7d8
ornob90 <oornob49@gmail.com>
2023-10-18 06:48:34 +0600

    setting up mongoDB uri

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ app.use(express.json());
 
 const port = process.env.PORT || 5001;
 
-const uri =
-  "mongodb+srv://ornonornob:<password>@cluster0.4hptkj7.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4hptkj7.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
