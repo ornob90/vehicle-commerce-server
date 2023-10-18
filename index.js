8ff1883594ff553ea95f17af7b71011496afdea0
ornob90 <oornob49@gmail.com>
2023-10-18 07:59:37 +0600

    get product details by id

--- index.js
+++ index.js
@@ -50,6 +50,15 @@ async function run() {
       res.send(result);
     });
 
+    // get single product details
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+
+      res.send(result);
+    });
+
     // post single product
     app.post("/product", async (req, res) => {
       const product = req.body;
@@ -67,7 +76,7 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/product/:id", async (req, res) => {
+    app.delete("/products/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await productCollection.deleteOne(query);
